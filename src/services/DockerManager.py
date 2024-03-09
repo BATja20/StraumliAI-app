eeb257c48bd028caea70cf8d839d248113035555
BATja20 <lucageorgelucian@gmail.com>
2024-03-09 23:52:05 +0200

    Fixed basic example

--- src/services/DockerManager.py
+++ src/services/DockerManager.py
@@ -1,6 +1,7 @@
 import logging
 import docker
 import time
+import os
 from docker.models.containers import Container
 from docker.models.networks import Network
 from docker.errors import APIError
@@ -25,27 +26,37 @@ class DockerManager:
             logging.error(f"Failed to remove network {network.id}: {e}")
             raise
         
-    def run_container(self, name: str, network_name: str, image: str = "alpine:latest", host_name: str = None) -> Container:
+    def run_container(self, name: str, network_name: str, network: Network, host_name: str = None) -> Container:
         try:
-            self.client.containers.run(image = image, name = name, hostname = host_name or name, detach = True, network = network_name)
+            command = "/bin/sh -c 'while true; do sleep 1; done'"
+            container = self.client.containers.run(
+                image = "alpine:latest",
+                name = name,
+                hostname = host_name or name,
+                detach = True,
+                network = network_name,
+                command = command)
+            logging.info(f"Started running container {container.id}")
+            return container
         except Exception as e:
             logging.error(f"Failed to run container {name}: {e}")
             raise
     
-    def remove_all_containers_from_network(self, network: Network):
+    def remove_all_containers_from_network(self, network_id: str):
+        network = self.client.networks.get(network_id = network_id)
+        logging.info(f"Containers to remove {network.containers}")
         for container in network.containers:
             container.stop()
-            container.remove() 
-            network.disconnect(container = container, force = True)
+            self.remove_container(container = container)
         logging.info(f"All containers removed from {network.id}")
         
     def network_has_no_active_endpoints(self, network: Network) -> bool:
         try:
             return bool(not network.attrs['Containers']) 
         except APIError as e:
-               logging.error(f"Error checking active endpoints for network {network.id}: {e}")
-               return False
-           
+            logging.error(f"Error checking active endpoints for network {network.id}: {e}")
+            return False
+    
     def wait_for_no_active_endpoints(self, network: Network, timeout: int = 30, interval = 2):
         end_time = time.time() + timeout
         while time.time() < end_time:
@@ -57,15 +68,13 @@ class DockerManager:
         logging.error(f"Timeout waiting for network {network.id} to have no active endpoints")
         return False
     
-    def safely_remove_network(self, network: Network) -> None:
-        time.sleep(1)
+    def safely_remove_network(self, network_id: str):
+        network = self.client.networks.get(network_id = network_id)
         if self.wait_for_no_active_endpoints(network = network):
             self.remove_network(network = network)
         else:
             logging.error(f"Network {network.id} could not be removed due to active endpoints or it timed out while trying to check for active endpoints")
-
-
-                
+  
     def remove_container(self, container: Container) -> None:
         if container is None:
             logging.warning("Attempted to remove a None container!!!")
@@ -73,10 +82,15 @@ class DockerManager:
         container.remove(force = True)
         
     def test_ping(self, from_container: Container, target_name: str) -> bool:
+        container = self.client.containers.get(from_container.id)
         try:
-            result = from_container.exec_run(f"ping -c 3 {target_name}")
+            if container.status != "running":
+                logging.error(f"Container {from_container.id} is not running.")
+                return False
+            result = container.exec_run(f"ping -c 3 {target_name}")
             success = "0% packet loss" in result.output.decode()
-            logging.info(f"Ping from {from_container.name} to {target_name} {'succeeded' if success else 'failed'}")
-        except docker.errors.APIError as e:
-            logging.error(f"Failed to ping from {from_container.name} to {target_name}: {e.explanation}")
+            logging.info(f"Ping from {container.id} to {target_name} {'succeeded' if success else 'failed'}")
+            return success
+        except APIError as e:
+            logging.error(f"Failed to ping from {container.id} to {target_name}: {e.explanation}")
             raise
