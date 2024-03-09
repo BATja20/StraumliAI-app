32b92eb9d6cbeb8b69b912b479937433af0f0e9b
BATja20 <lucageorgelucian@gmail.com>
2024-03-10 00:59:15 +0200

    Used black to format

--- src/CLI.py
+++ src/CLI.py
@@ -2,52 +2,74 @@ import argparse
 import logging
 from services.DockerManager import DockerManager
 
+
 class CLI:
-    
-    NETWORK_NAME = 'hack-net'
-    
+
+    NETWORK_NAME = "hack-net"
+
     def __init__(self):
         self.manager = DockerManager()
-    
+
     @staticmethod
     def parse_args() -> argparse.Namespace:
-        parser = argparse.ArgumentParser(description="Manages Docker containers on a network")
-        parser.add_argument("--targets", type=int, help = "Number of target containers to create", required = True)
+        parser = argparse.ArgumentParser(
+            description="Manages Docker containers on a network"
+        )
+        parser.add_argument(
+            "--targets",
+            type=int,
+            help="Number of target containers to create",
+            required=True,
+        )
         return parser.parse_args()
-    
+
     @staticmethod
     def check_args(args: argparse.Namespace):
-        if (args.targets <= 0):
-            raise argparse.ArgumentTypeError(f"Argument 'target' must be greater than 0, given value: {args.target}")
-    
+        if args.targets <= 0:
+            raise argparse.ArgumentTypeError(
+                f"Argument 'target' must be greater than 0, given value: {args.target}"
+            )
+
     def run_pipeline(self, targets_count: int) -> bool:
         is_success = False
         try:
-            network = self.manager.create_network(network_name = self.NETWORK_NAME)
-            attacker = self.manager.run_container("attacker", network_name = self.NETWORK_NAME, network = network)
-            targets = [self.manager.run_container(f"target-{i}", self.NETWORK_NAME, network = network) for i in range(targets_count)]
+            network = self.manager.create_network(network_name=self.NETWORK_NAME)
+            attacker = self.manager.run_container(
+                "attacker", network_name=self.NETWORK_NAME, network=network
+            )
+            targets = [
+                self.manager.run_container(
+                    f"target-{i}", self.NETWORK_NAME, network=network
+                )
+                for i in range(targets_count)
+            ]
             targets = [target for target in targets if target is not None]
-            
-            is_success = all(self.manager.test_ping(attacker, target_name = target.attrs["Name"][1:]) for target in targets) # [1:] is to remove the leading slash
-            logging.info(f"Ping Test Results: {'Successful' if is_success else 'Failed'}")
+
+            is_success = all(
+                self.manager.test_ping(attacker, target_name=target.attrs["Name"][1:])
+                for target in targets
+            )  # [1:] is to remove the leading slash
+            logging.info(
+                f"Ping Test Results: {'Successful' if is_success else 'Failed'}"
+            )
             return is_success
         except Exception as e:
             logging.error(f"Error encountered while running pipeline: {e}")
             return False
         finally:
-            self.manager.remove_all_containers_from_network(network_id = network.id)
-            self.manager.safely_remove_network(network_id = network.id)
+            self.manager.remove_all_containers_from_network(network_id=network.id)
+            self.manager.safely_remove_network(network_id=network.id)
             return is_success
-            
-    
+
     def run(self) -> None:
         args = self.parse_args()
-        self.check_args(args = args)
-        success = self.run_pipeline(targets_count = args.targets)
+        self.check_args(args=args)
+        success = self.run_pipeline(targets_count=args.targets)
         exit(0 if success else 1)
-    
-if  __name__ == "__main__":
-    logging.basicConfig(level = logging.INFO)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     manager = DockerManager()
     cli = CLI()
     cli.run()
--- src/services/DockerManager.py
+++ src/services/DockerManager.py
@@ -5,79 +5,93 @@ from docker.models.containers import Container
 from docker.models.networks import Network
 from docker.errors import APIError
 
+
 class DockerManager:
     NETWORK_DRIVER_TYPE = "bridge"
-    
+
     def __init__(self):
         self.client = docker.from_env()
-        
+
     def create_network(self, network_name: str) -> Network:
         try:
-            return self.client.networks.create(network_name, driver = self.NETWORK_DRIVER_TYPE)
+            return self.client.networks.create(
+                network_name, driver=self.NETWORK_DRIVER_TYPE
+            )
         except Exception as e:
             logging.error(f"Failed to create network {network_name}: {e}")
             raise
-    
+
     def remove_network(self, network: Network):
         try:
             network.remove()
         except Exception as e:
             logging.error(f"Failed to remove network {network.id}: {e}")
-        
-    def run_container(self, name: str, network_name: str, network: Network, host_name: str = None) -> Container:
+
+    def run_container(
+        self, name: str, network_name: str, network: Network, host_name: str = None
+    ) -> Container:
         try:
             command = "/bin/sh -c 'while true; do sleep 1; done'"
             container = self.client.containers.run(
-                image = "alpine:latest",
-                name = name,
-                hostname = host_name or name,
-                detach = True,
-                network = network_name,
-                command = command)
+                image="alpine:latest",
+                name=name,
+                hostname=host_name or name,
+                detach=True,
+                network=network_name,
+                command=command,
+            )
             logging.info(f"Started running container {container.id}")
             return container
         except Exception as e:
             logging.error(f"Failed to run container {name}: {e}")
             return None
-    
+
     def remove_all_containers_from_network(self, network_id: str):
-        network = self.client.networks.get(network_id = network_id)
+        network = self.client.networks.get(network_id=network_id)
         for container in network.containers:
             container.stop()
-            self.remove_container(container = container)
+            self.remove_container(container=container)
         logging.info(f"All containers removed from {network.id}")
-        
+
     def network_has_no_active_endpoints(self, network: Network) -> bool:
         try:
-            return bool(not network.attrs['Containers']) 
+            return bool(not network.attrs["Containers"])
         except APIError as e:
-            logging.error(f"Error checking active endpoints for network {network.id}: {e}")
+            logging.error(
+                f"Error checking active endpoints for network {network.id}: {e}"
+            )
             return False
-    
-    def wait_for_no_active_endpoints(self, network: Network, timeout: int = 30, interval = 2):
+
+    def wait_for_no_active_endpoints(
+        self, network: Network, timeout: int = 30, interval=2
+    ):
         end_time = time.time() + timeout
         while time.time() < end_time:
-            if self.network_has_no_active_endpoints(network = network):
+            if self.network_has_no_active_endpoints(network=network):
                 return True
             logging.info("Waiting for network to clear active endpoints...")
             time.sleep(interval)
-        logging.error(f"Timeout waiting for network {network.id} to have no active endpoints")
+        logging.error(
+            f"Timeout waiting for network {network.id} to have no active endpoints"
+        )
         return False
-    
+
     def safely_remove_network(self, network_id: str):
-        network = self.client.networks.get(network_id = network_id)
-        if self.wait_for_no_active_endpoints(network = network):
-            self.remove_network(network = network)
+        network = self.client.networks.get(network_id=network_id)
+        if self.wait_for_no_active_endpoints(network=network):
+            self.remove_network(network=network)
         else:
-            logging.error(f"Network {network.id} could not be removed due to active endpoints or it timed out while trying to check for active endpoints")
-  
+            logging.error(
+                f"Network {network.id} could not be removed due to active endpoints or it timed out while trying to check for active endpoints"
+            )
+
     def remove_container(self, container: Container):
         try:
-            container.remove(force = True)
+            container.remove(force=True)
             logging.info(f"Successfully removed container: {container.id}")
         except APIError as e:
             logging.error(f"Failed to remove container {container.id}: {e}")
-        
+
     def test_ping(self, from_container: Container, target_name: str) -> bool:
         container = self.client.containers.get(from_container.id)
         try:
@@ -86,7 +100,9 @@ class DockerManager:
                 return False
             result = container.exec_run(f"ping -c 3 {target_name}")
             success = "0% packet loss" in result.output.decode()
-            logging.info(f"Ping from {container.name} to {target_name} {'succeeded' if success else 'failed'}")
+            logging.info(
+                f"Ping from {container.name} to {target_name} {'succeeded' if success else 'failed'}"
+            )
             return success
         except APIError as e:
             logging.error(f"Failed to ping from {container.id} to {target_name}: {e}")
